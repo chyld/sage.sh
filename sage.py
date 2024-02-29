9ba0d8d50f5dbe7579fbf24d98e684171b4d841a
Chyld Medford <chyld.medford@gmail.com>
2024-02-29 09:22:13 -0800

    saving responses

--- sage.py
+++ sage.py
@@ -9,12 +9,12 @@ content = sys.stdin.read()
 response = client.chat.completions.create(
   model="gpt-4-0125-preview", # https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
   messages=[
-    {"role": "system", "content": "You are a zsh and bash shell expert on linux, please help me at the terminal complete the following command, you should output the completed command with a few different examples. Be concise. Always use markdown syntax for your response. Please use markdown."},
+    {"role": "system", "content": "You are a zsh and bash shell expert on linux, please help me at the terminal complete the following command, you should output the completed command with a few different examples. Be concise. Always use markdown syntax for your response. Please use markdown and have samples of code or terminal commands with fenced code blocks and syntax highlighting."},
     {"role": "user", "content": content}
   ]
 )
 
-print(f"# {content}")
+print(f"INPUT\n{content}\n\nOUTPUT")
 print(response.choices[0].message.content)
 print("# end of response")
 
--- sage.sh
+++ sage.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 shellsage() {
-    local input="$*"
-    local tempfile="/tmp/mytempfile.$$.$RANDOM.md"
-    echo -n "$input" | sage.py 1> "$tempfile"
-    glow "$tempfile"
-    rm "$tempfile"
+    local prompt="$*"
+    local filename="$HOME/Documents/sage.sh/$(date '+%Y-%m-%d-%H-%M-%S').md"
+    echo -n "$prompt" | sage.py 1> "$filename"
+    bat --style full "$filename"
 }
 
 alias ss=shellsage
