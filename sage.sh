fb955210cbcb63c758be02e5c28c65927e92bbc7
Chyld Medford <chyld.medford@gmail.com>
2024-02-29 16:08:32 -0800

    exit function if file is empty

--- sage.sh
+++ sage.sh
@@ -4,6 +4,7 @@ shellsage() {
     local infile="$HOME/Documents/sage.sh/$(date '+%Y-%m-%d-%H-%M-%S').in.md"
     local outfile="$HOME/Documents/sage.sh/$(date '+%Y-%m-%d-%H-%M-%S').out.md"
     nvim "$infile"
+    if [ ! -s "$infile" ]; then echo "The file is empty. Nothing to do."; return 1; fi
     bat "$infile" | sage.py 1> "$outfile"
     bat --style full "$outfile"
 }
