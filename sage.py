6d509795c78b67c2185e6d915225b13cb9eeda9d
Chyld Medford <chyld.medford@gmail.com>
2024-03-01 03:05:31 -0800

    separating out the system prompt

--- sage.py
+++ sage.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
+import os
 import sys
 from openai import OpenAI
 
 client = OpenAI()
-content = sys.stdin.read()
+prompt = sys.stdin.read()
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+instructions_path = os.path.join(script_dir, 'sage.instructions')
+with open(instructions_path, 'r') as f:
+    instructions = f.read()
 
 response = client.chat.completions.create(
   model="gpt-4-0125-preview", # https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
   messages=[
-    {"role": "system", "content": "As a developer navigating the terminal, I seek guidance primarily in zsh and bash shell usage on Linux, alongside Python and JavaScript expertise. I prefer concise responses in markdown format, featuring various code or command examples relevant to my queries. Here are the guidelines. 1. Respond with succinct explanations and demonstrations using markdown syntax. 2. Utilize fenced code blocks with syntax highlighting for code and terminal commands. 3. Provide diverse examples to enrich understanding."},
-    {"role": "user", "content": content}
+    {"role": "system", "content": instructions},
+    {"role": "user", "content": prompt}
   ]
 )
 
-print(f"INPUT\n{content}\n\nOUTPUT")
+print(f"INPUT\n> instructions\n\n{instructions}\n> prompt\n\n{prompt}\n\nOUTPUT")
 print(response.choices[0].message.content)
-print("# end of response")
+print("\n\n# end of response")
 
